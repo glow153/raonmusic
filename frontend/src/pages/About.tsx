58301a0cd30adb63145f1b251292ddcd93b99cc8
Jake Park <glow153@gmail.com>
2022-12-12 20:14:39 +0900

    etc.

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 
 const About = () => {
   return (
     <div>
       <h1>소개</h1>
-      <p>이 프로젝트는 리액트 라우터 기초를 실습해 보는 예제 프로젝트입니다.</p>
+      <p>라온데이터는 짱입니다.</p>
     </div>
   );
 };
